e6a959936007bf7455d39fb76671e375d71cd4aa
norbert barics <norbert.barics@gmail.com>
2017-12-10 23:12:52 +0100

    add solution 57

--- src/Problems/problem57/Problem57.java
+++ src/Problems/problem57/Problem57.java
@@ -1,18 +1,43 @@
 package Problems.problem57;
 
+import java.math.BigInteger;
+
+/**
+ * 3/2, 7/5, 17/12, 41/29, 99/70, 239/169
+ */
+
 public class Problem57 {
 
-  public double problem57Solution() {
-    return getExpansions(2);
+  private BigInteger numerator = BigInteger.valueOf(3);
+  private BigInteger denominator = BigInteger.valueOf(2);
+  private BigInteger numTemp = BigInteger.ONE;
+  private BigInteger denTemp = BigInteger.ONE;
+
+  public int problem57Solution() {
+    return getBruteForceSolution(1000);
   }
 
-  private double getExpansions(int endIndex) {
-    double number = 0;
-    double temp = 0.5;
-    for (int i = 0; i < endIndex; i++) {
-      number = 1 / (2 + temp);
-      temp = number;
+  private int getBruteForceSolution(int index) {
+    int counter = 0;
+    for (int i = 0; i < index; ++i) {
+      if (getFractions()) {
+        counter++;
+      }
     }
-    return 1 + number;
+    return counter;
+  }
+
+  private boolean getFractions() {
+    BigInteger num = numerator;
+    BigInteger den = denominator;
+    numerator = numerator.add(numerator).add(numTemp);
+    denominator = denominator.add(denominator).add(denTemp);
+    numTemp = num;
+    denTemp = den;
+    return countDigits(numerator, denominator);
+  }
+
+  private boolean countDigits(BigInteger num1, BigInteger num2) {
+  return num1.toString().length() > num2.toString().length();
   }
 }
