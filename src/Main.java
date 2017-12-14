f37929e82911450a32fb6fe919359fed7661e9d4
norbert barics <norbert.barics@gmail.com>
2017-12-14 21:22:14 +0100

    add task 58 solution

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import Problems.problem57.Problem57;
+import Problems.Problem58.Problem58;
 
 /**
 project euler 50-100
  */
 public class Main {
   public static void main(String[] args) {
-    Problem57 problem57 = new Problem57();
-    System.out.println(problem57.problem57Solution());
+    Problem58 problem58 = new Problem58();
+    System.out.println(problem58.problem58Solution());
   }
 }
